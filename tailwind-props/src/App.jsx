e9e6d6de706970dfaaa75e432be0fd0a734d5509
WasiQ-dev <muhammadwasiq67585@gmail.com>
2025-04-30 13:40:51 +0500

    props working

--- tailwind-props/src/App.jsx
+++ tailwind-props/src/App.jsx
@@ -6,11 +6,13 @@ import Card from './components/card'
 
 function App() {
   const [count, setCount] = useState(0)
+ 
 
   return (
     <>
       <h1 className='bg-green-500 text-center p-4 text-black rounded-xl mb-6'>Tailwand test</h1>
-      <Card/>
+      <Card username="The Cold Mountains" headingtext="Clip Board"/>
+      <Card username="The Road Way" headingtext="Surronding Environmnet"/>
     </>
   )
 }
