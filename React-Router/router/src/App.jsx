fd8bf6e2c4b29e0d3503c051f62afeb9fe8cc135
WasiQ-dev <muhammadwasiq67585@gmail.com>
2025-05-06 01:39:38 +0500

    React-Router

---
+++ React-Router/router/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/header/Header'
+import Home from './components/Home/Home'
+import Footer from'./components/footer/Footer'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Header/>
+    <Home/>
+    <Footer/>
+    </>
+   
+  )
+}
+
+export default App
