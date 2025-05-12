4202ed5c7d4e6e6d226fe7b571663df5dc3c9cc9
WasiQ-dev <muhammadwasiq67585@gmail.com>
2025-05-12 16:59:06 +0500

    todo

---
+++ Todo App/todo-app/src/Contexts/Context.js
@@ -0,0 +1,19 @@
+import { createContext, useContext } from "react";
+export const TodoContext = createContext({
+    todos:[
+        {
+            id:1,
+            todo:"todo-msg",
+            completed:false,
+        }
+    ],
+    addTodo:(todo)=>{},
+    deleteTodo:(id)=>{},
+    updateTodo:(id,todo)=>{},
+    toggleTodo:(id)=>{},
+
+});
+export const useTodo = () => {
+  return useContext(TodoContext);
+};
+export const todoProvider = TodoContext.Provider;
---
+++ Todo App/todo-app/src/Contexts/Index.js
@@ -0,0 +1,1 @@
+import { useTodo,todoProvider,TodoContext } from "./Context"; 
