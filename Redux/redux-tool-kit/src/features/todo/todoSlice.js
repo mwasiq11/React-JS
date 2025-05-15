dad8c17e39b0df34545e33c363e6bb6b3af1f421
WasiQ-dev <muhammadwasiq67585@gmail.com>
2025-05-16 03:38:52 +0500

    making Store/reducers

--- Redux/redux-tool-kit/src/features/todo/todoSlice.js
+++ Redux/redux-tool-kit/src/features/todo/todoSlice.js
@@ -1,19 +1,29 @@
-import {createSlice,nanoid} from "@reduxjs/toolkit"
-const initialState={
-   todos:[{
-    id:1,
-    text:"Learn Redux",
-   }]
-}
-const todoSlice=createSlice({
-    name:"todo",
-    initialState,
-    reducers:{
-        addTodo:(state,action)=>{},
-        removeTodo:()=>{},
-    }
-    
-})
-       
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 
+const initialState = {
+  todos: [
+    {
+      id: 1,
+      text: "Learn Redux",
+    },
+  ],
+};
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addtodo: (state, action) => {
+        const todo={
+            id:nanoid(),
+            text:action.payload,
+        }
+        state.todos.push(todo)
+    },
+    removetodo: (state,action) => {
+        state.todos=state.todos.filter((todo)=>{todo.id!==action.payload})
+    },    
+  },
+});
 
+export const {addtodo,removetodo}=todoSlice.actions
+export default todoSlice.reducer
