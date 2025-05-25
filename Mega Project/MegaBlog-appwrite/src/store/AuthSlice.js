517d78fea377b76b6e3922f68ad7c4ed68206471
WasiQ-dev <muhammadwasiq67585@gmail.com>
2025-05-25 17:37:47 +0500

    creating-store

---
+++ Mega Project/MegaBlog-appwrite/src/store/AuthSlice.js
@@ -0,0 +1,23 @@
+import { createSlice} from "@reduxjs/toolkit";
+
+const initialState={
+    status:false,
+    userData:null,
+}
+const AuthSlice=createSlice({
+    name:"Auth",
+    initialState,
+    reducers:{
+        login:(state,action)=>{
+            state.status=true,
+            action.userData=action.payload.userData
+        },
+        logout:(state)=>{
+            state.status=false,
+            state.userData=null
+        },
+    }
+})
+export const {login,logout}= AuthSlice.actions;
+export default AuthSlice.reducer;
+
