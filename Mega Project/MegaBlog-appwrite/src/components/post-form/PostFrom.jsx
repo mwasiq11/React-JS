261f80b77e227512b79b44084bc95ad8575db92b
WasiQ-dev <muhammadwasiq67585@gmail.com>
2025-06-20 22:51:38 +0500

    Working on PostFrom if user don't get Post

--- Mega Project/MegaBlog-appwrite/src/components/post-form/PostFrom.jsx
+++ Mega Project/MegaBlog-appwrite/src/components/post-form/PostFrom.jsx
@@ -4,13 +4,13 @@ import { Button, Input, RTE, Select } from "..";
 import { useNavigate } from "react-router-dom";
 import appwriteService from "../../Appwrite/config";
 import { useSelector } from "react-redux";
-import { useForm } from "react-hook-form";  // hook that is used to add forms//
+import { useForm } from "react-hook-form"; // hook that is used to add forms//
 
 function PostFrom({ post }) {
   const { register, handleSubmit, watch, getValues, setValue, control } =
     useForm({
       defaultValues: {
-        title: post?.title || "", 
+        title: post?.title || "",
         slug: post?.slug || "",
         content: post?.content || "",
         status: post?.status || "",
@@ -21,7 +21,7 @@ function PostFrom({ post }) {
 
   const submit = async (data) => {
     if (post) {
-      const file = data.image[0]   // if get image at 0 index use appwrite service to upload the file at of index 0 //
+      const file = data.image[0] // if get image at 0 index use appwrite service to upload the file at of index 0 //
         ? appwriteService.uploadFile(data.image[0])
         : null;
       if (file) {
@@ -30,10 +30,25 @@ function PostFrom({ post }) {
       const dbPost = await appwriteService.updatePost(post.$id, {
         ...data,
         featuredImage: file ? file.$id : undefined,
-        if(dbPost) {
-          navigate(`/post/${dbPost.id}`);
-        },
       });
+      if (dbPost) {
+        navigate(`/post/${dbPost.id}`);
+      }
+      // use case of if not get the post//
+      else {
+        const file = await appwriteService.uploadFile(data.image[0]);
+        if (file) {
+          const fileId = file.$id;
+          data.featuredImage = fileId;
+          const dbPost = appwriteService.createPost({
+            ...data,
+            userId: userData.$id,
+          });
+          if (dbPost) {
+            navigate(`/post/${dbPost.$id}`);
+          }
+        }
+      }
     }
   };
 
