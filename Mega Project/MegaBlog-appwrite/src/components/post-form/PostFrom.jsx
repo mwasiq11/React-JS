0d2452712617f1a37b3bc8e2c889a4e41fe3bb85
WasiQ-dev <muhammadwasiq67585@gmail.com>
2025-06-26 04:05:35 +0500

    Creating Slug Transform

--- Mega Project/MegaBlog-appwrite/src/components/post-form/PostFrom.jsx
+++ Mega Project/MegaBlog-appwrite/src/components/post-form/PostFrom.jsx
@@ -51,6 +51,17 @@ function PostFrom({ post }) {
       }
     }
   };
+  const slugTransforam=useCallback((value)=>{
+    if (value && typeof(value)==="string") {
+      return value
+      .trim()
+      .toLowerCase
+      .replace(/^[a-zA-Z\d\s]+/g,'-')
+      .replace(/\s/g,'-')
+      
+    }
+    return ""
+  },[])
 
   return <div>Post-From</div>;
 }
