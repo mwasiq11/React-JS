b2904f2a5f5ffc59625e16c042ffb6e5df692ba7
WasiQ-dev <muhammadwasiq67585@gmail.com>
2025-07-01 02:20:23 +0500

    Finalizing PostFrom

--- Mega Project/MegaBlog-appwrite/src/components/post-form/PostFrom.jsx
+++ Mega Project/MegaBlog-appwrite/src/components/post-form/PostFrom.jsx
@@ -51,19 +51,88 @@ function PostFrom({ post }) {
       }
     }
   };
-  const slugTransforam=useCallback((value)=>{
-    if (value && typeof(value)==="string") {
+  const slugTransform = useCallback((value) => {
+    if (value && typeof value === "string") {
       return value
-      .trim()
-      .toLowerCase
-      .replace(/^[a-zA-Z\d\s]+/g,'-')
-      .replace(/\s/g,'-')
-      
+        .trim()
+        .toLowerCase.replace(/^[a-zA-Z\d\s]+/g, "-")
+        .replace(/\s/g, "-");
     }
-    return ""
-  },[])
-
-  return <div>Post-From</div>;
+    return "";
+  }, []);
+  //  also a interview question//
+  React.useEffect(() => {
+    const subscription = watch((value, { name }) => {
+      if (name === "title") {
+        setValue("slug", slugTransform(value.title, { shouldValidate: true }));
+      }
+    });
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [slugTransform, setValue, watch]);
+  return (
+    <form onSubmit={handleSubmit(submit)} className="flex flex-wrap">
+      //left side of the form //
+      <div className="w-2/3 px-2">
+        <Input
+          label="Title :"
+          placeholder="Title"
+          className="mb-4"
+          {...register("title", { required: true })}
+        />
+        <Input
+          label="Slug :"
+          placeholder="Slug"
+          className="mb-4"
+          {...register("slug", { required: true })}
+          onInput={(e) => {
+            setValue("slug", slugTransform(e.currentTarget.value), {
+              shouldValidate: true,
+            });
+          }}
+        />
+        <RTE
+          label="Content :"
+          name="content"
+          control={control}
+          defaultValue={getValues("content")}
+        />
+      </div>
+      //right side of the form //
+      <div className="w-1/3 px-2">
+        <Input
+          label="Featured Image :"
+          type="file"
+          className="mb-4"
+          accept="image/png, image/jpg, image/jpeg, image/gif"
+          {...register("image", { required: !post })}
+        />
+        {post && (
+          <div className="w-full mb-4">
+            <img
+              src={appwriteService.getFilePreview(post.featuredImage)}
+              alt={post.title}
+              className="rounded-lg"
+            />
+          </div>
+        )}
+        <Select
+          options={["active", "inactive"]}
+          label="Status"
+          className="mb-4"
+          {...register("status", { required: true })}
+        />
+        <Button
+          type="submit"
+          bgColor={post ? "bg-green-500" : undefined}
+          className="w-full"
+        >
+          {post ? "Update" : "Submit"}
+        </Button>
+      </div>
+    </form>
+  );
 }
 
 export default PostFrom;
