c01b98533d18055ec1f2283bad346885739b85d6
WasiQ-dev <muhammadwasiq67585@gmail.com>
2025-07-02 02:38:34 +0500

    Working on Pages:/Editpost/Home

---
+++ Mega Project/MegaBlog-appwrite/src/pages/Editpost.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { Container, PostFrom } from "../components";
+import appwriteService from "../appwrite/config";
+import { useParams, useNavigate } from "react-router-dom";
+
+function Editpost() {
+  const [post, setPost] = useState(null);
+  const { slug } = useParams();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (post) {
+      appwriteService.getPost(slug).then(() => {
+        if (post) {
+          setPost(post);
+        }
+      });
+    } else {
+      navigate("/");
+    }
+  }, [slug, navigate]);
+  return post ? (
+    <div className="py-8">
+      <Container>
+        <PostFrom post={post} />
+      </Container>
+    </div>
+  ) : null;
+}
+
+export default Editpost;
